6095c1741ecb377919c28d402716cf1d1ac27fbd
alessiobortone2 <alessiobortone2@gmail.com>
2017-09-22 21:23:51 +0100

    views added

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,56 @@ feature 'restaurants' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
-      expetc(page).to have_link 'Add a restaurant'
+      expect(page).to have_link 'Add a restaurant'
+    end
+  end
+
+  context 'restaurants have been added' do
+  before do
+    Restaurant.create(name: 'KFC')
+  end
+
+  scenario 'display restaurants' do
+    visit '/restaurants'
+    expect(page).to have_content('KFC')
+    expect(page).not_to have_content('No restaurants yet')
+  end
+  end
+  context 'creating restaurants' do
+  scenario 'prompts user to fill out a form, then displays the new restaurant' do
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    click_button 'Create Restaurant'
+    expect(page).to have_content 'KFC'
+    expect(current_path).to eq '/restaurants'
+  end
+  end
+
+  context 'viewing restaurants' do
+
+  let!(:kfc){ Restaurant.create(name:'KFC') }
+
+  scenario 'lets a user view a restaurant' do
+   visit '/restaurants'
+   click_link 'KFC'
+   expect(page).to have_content 'KFC'
+   expect(current_path).to eq "/restaurants/#{kfc.id}"
+  end
+  end
+
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      click_link 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants/1'
     end
   end
 end
